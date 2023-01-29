a6690e532eba92fbbaa75094f549797105df1619
Grzegorz Wozniak <grubysweter@gmail.com>
2023-01-29 10:56:43 +0100

    LoginPage

---
+++ news/src/components/HomePage/HomePage.tsx
@@ -0,0 +1,16 @@
+import {useEffect} from "react";
+import { Typography } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <Typography
+      variant="h2"
+      align="center"
+      sx={{ fontSize: "2rem", mt: ".8rem" }}
+    >
+      Today's hottest news
+    </Typography>
+  );
+};
+
+export default HomePage;
---
+++ news/src/helpers/api.ts
@@ -0,0 +1,1 @@
+export const API_KEY = "77a69ce3d3a84adb8e168394cb9ace38";
