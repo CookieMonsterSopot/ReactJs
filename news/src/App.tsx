9029274d4989759a44d8d480578110968ef59c82
Grzegorz Wozniak <grubysweter@gmail.com>
2023-02-03 13:06:21 +0100

    my-weather-app init

--- news/src/App.tsx
+++ news/src/App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import HomePage from "./components/HomePage/HomePage";
 import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./components/HomePage/HomePage";
 import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <RegisterForm />
-      {/* <LoginPage /> */}
-      <HomePage />
+      <BrowserRouter>
+        {/* STATIC */}
+        <Navbar />
+        <Routes>
+          {/* DYNAMIC */}
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+        {/* STATIC */}
+      </BrowserRouter>
     </div>
   );
 }
